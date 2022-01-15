eb2c53f1b6c625c3b248a8eb5e591b2586ffb4fa
Mackinnon Buck <mackinnon.buck@gmail.com>
2022-01-15 09:26:17 -0800

    Fixed drivetrain ports and controller axis readings. (#4)

--- src/main/java/frc/robot/Ids.java
+++ src/main/java/frc/robot/Ids.java
@@ -4,10 +4,10 @@ public final class Ids {
     public static final int CONTROLLER_DRIVE_PORT = 0;
     public static final int CONTROLLER_OPERATOR_PORT = 0;
 
-    public static final int DRIVE_LEFT_LEAD_DEVICE = 1;
-    public static final int DRIVE_LEFT_FOLLOW_DEVICE = 2;
-    public static final int DRIVE_RIGHT_LEAD_DEVICE = 4;
-    public static final int DRIVE_RIGHT_FOLLOW_DEVICE = 3;
+    public static final int DRIVE_LEFT_LEAD_DEVICE = 3;
+    public static final int DRIVE_LEFT_FOLLOW_DEVICE = 4;
+    public static final int DRIVE_RIGHT_LEAD_DEVICE = 1;
+    public static final int DRIVE_RIGHT_FOLLOW_DEVICE = 2;
 
     public static final int LIFT_WINCH_DEVICE = 5;
     public static final int LIFT_SOLENOID_FORWARD = 1;
--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -54,6 +54,8 @@ public class DriveSubsystem extends SubsystemBase {
 
         configureEncoder(leftEncoder);
         configureEncoder(rightEncoder);
+
+        rightMotors.setInverted(true);
     }
 
     @Override
