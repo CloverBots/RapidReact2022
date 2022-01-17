1c8480adefc049b8240ce58b9ebf9034dc236039
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2022-01-17 12:37:03 -0800

    Incorporated vision target into system (#9)
    * Incorporated vision target into system
    
    * Update src/main/java/frc/robot/VisionTargetTracker.java
    
    Co-authored-by: Mackinnon Buck <mackinnon.buck@gmail.com>

--- src/main/java/frc/robot/commands/ShooterCommand.java
+++ src/main/java/frc/robot/commands/ShooterCommand.java
@@ -1,6 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.VisionTargetTracker;
+import frc.robot.VisionTargetTracker.LedMode;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class ShooterCommand extends CommandBase {
@@ -8,29 +10,49 @@ public class ShooterCommand extends CommandBase {
     private static final double SHOOTER_SPEED = 0.5; // TODO Find shooter speed
 
     private final ShooterSubsystem shooterSubsystem;
+    private final VisionTargetTracker visionTarget;
 
     /** Creates a new ShootBall. */
-    public ShooterCommand(ShooterSubsystem shooterSubsystem) {
+    public ShooterCommand(ShooterSubsystem shooterSubsystem, VisionTargetTracker visionTarget) {
         this.shooterSubsystem = shooterSubsystem;
-        addRequirements(shooterSubsystem);
+        this.visionTarget = visionTarget;
 
-        // Use addRequirements() here to declare subsystem dependencies.
+        //By adding requirement to the shooterSubsystem, WPI will ensure that if another
+        // system is using the motors, this command will supersede control of the motor.
+        // This prevents multiple commands from trying to control the same motor.
+        addRequirements(shooterSubsystem);
     }
 
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        visionTarget.setLedMode(LedMode.FORCE_ON);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        shooterSubsystem.startShooter(SHOOTER_SPEED);
+
+        if (visionTarget.isValid()) {
+            // Distance is in inches
+            double distance = visionTarget.computeTargetDistance();
+            //System.out.println(distance);
+
+            // Dummy formula for testing
+            if (distance > 100) {
+                shooterSubsystem.startShooter(SHOOTER_SPEED);
+            } else {
+                shooterSubsystem.stop();
+            }
+        } else {
+            shooterSubsystem.stop();
+        }
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        visionTarget.setLedMode(LedMode.FORCE_OFF);
         shooterSubsystem.stop();
     }
 
