e17a5be3b70cd01b65e5382f28f3f94c5e480ac0
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2022-01-27 18:29:43 -0800

    Made variables private

--- src/main/java/frc/robot/subsystems/FeederSubsystem.java
+++ src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -38,19 +38,19 @@ public class FeederSubsystem extends SubsystemBase {
      * 0,1,2 or 3. Only the first two (0,1) are visible in web-based
      * configuration.
      */
-    public static final int kSlotIdx = 0;
+    private static final int kSlotIdx = 0;
 
     /**
      * Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
      * now we just want the primary one.
      */
-    public static final int kPIDLoopIdx = 0;
+    private static final int kPIDLoopIdx = 0;
 
     /**
      * Set to zero to skip waiting for confirmation, set to nonzero to wait and
      * report to DS if action fails.
      */
-    public static final int kTimeoutMs = 30;
+    private static final int kTimeoutMs = 30;
 
     /**
      * PID Gains may have to be adjusted based on the responsiveness of control
@@ -60,7 +60,7 @@ public class FeederSubsystem extends SubsystemBase {
      * 
      * kP kI kD kF Iz PeakOut
      */
-    public final Gains kGains_Velocit = new Gains(0.25, 0.001, 20, 1023.0 / 7200.0, 300, 1.00);
+    private final Gains kGains_Velocit = new Gains(0.25, 0.001, 20, 1023.0 / 7200.0, 300, 1.00);
 
     TalonSRX talonLead = new WPI_TalonSRX(1);
 
@@ -110,5 +110,4 @@ public class FeederSubsystem extends SubsystemBase {
     public void stopFeeder() {
         talonLead.set(ControlMode.Velocity, 0);
     }
-
 }
