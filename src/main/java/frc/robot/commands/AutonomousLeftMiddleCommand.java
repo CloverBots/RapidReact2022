1093455becfbdb95a60d38fb047a3f6d8cc50293
savortheflavor <savortheflavor.gg@gmail.com>
2022-01-29 10:37:47 -0800

    Autonomous Left Middle

--- src/main/java/frc/robot/commands/AutonomousLeftMiddleCommand.java
+++ src/main/java/frc/robot/commands/AutonomousLeftMiddleCommand.java
@@ -1,9 +1,11 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.SequentialCommandGroupExtended;
 import frc.robot.VisionTargetTracker;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.FeederSubsystem;
+import frc.robot.subsystems.IntakeDeploySubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
@@ -15,17 +17,21 @@ public class AutonomousLeftMiddleCommand extends SequentialCommandGroupExtended 
     /** Creates a new AutonomousLM. */
     public AutonomousLeftMiddleCommand(DriveSubsystem driveSubsystem,
         IntakeSubsystem intakeSubsystem, 
+        IntakeDeploySubsystem intakeDeploySubsystem,
         FeederSubsystem feederSubsystem,
         ShooterSubsystem shooterSubsystem,
         VisionTargetTracker visionTargetTracker) {
         
         //   Autonomous commands in running order
+        addInstant(() -> intakeDeploySubsystem.setSolenoid(true), intakeDeploySubsystem); 
         addInstant(() -> intakeSubsystem.startIntake(), intakeSubsystem);
         addCommands(new DriveToDistanceCommand(driveSubsystem, DRIVE_DISTANCE, DRIVE_SPEED));
         addCommands(new AimHighCommand(driveSubsystem, visionTargetTracker));
         addCommands(new ShooterCommand(shooterSubsystem, visionTargetTracker));
         addInstant(() -> feederSubsystem.startFeeder(), feederSubsystem);
+        addCommands(new WaitCommand(3));
+        addInstant(() -> feederSubsystem.stopFeeder(), feederSubsystem);
         addInstant(() -> intakeSubsystem.stop(), intakeSubsystem);
-
+        addInstant(() -> intakeDeploySubsystem.setSolenoid(false), intakeDeploySubsystem);
     }
 }
