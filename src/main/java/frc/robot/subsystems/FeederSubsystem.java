31e829997cb26e10111be6ef8781463129468c62
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2022-02-03 19:46:19 -0800

    Added feeder logic and controls (#43)
    Updated feeder subsystem with known hardware and added logic functions for controlling the movement of the balls through the feeder. Changed so intake and feeder run together. Changed so shooter and feeder run together. Autonomous using feeders will need to be changed.
    
    Co-authored-by: NeuralNetBot <console.logisaiah@gmail.com>

--- src/main/java/frc/robot/subsystems/FeederSubsystem.java
+++ src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -5,90 +5,22 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FeederSubsystem extends SubsystemBase {
 
-    private static final double DEFAULT_FEEDER_RPM = 10; //TODO get real value
+    TalonSRX upperFeeder = new WPI_TalonSRX(1);
+    TalonSRX lowerFeeder = new WPI_TalonSRX(1);
 
-    private class Gains {
-        public final double kP;
-        public final double kI;
-        public final double kD;
-        public final double kF;
-        public final int kIzone;
-        public final double kPeakOutput;
-
-        public Gains(double _kP, double _kI, double _kD, double _kF, int _kIzone, double _kPeakOutput) {
-            kP = _kP;
-            kI = _kI;
-            kD = _kD;
-            kF = _kF;
-            kIzone = _kIzone;
-            kPeakOutput = _kPeakOutput;
-        }
-    }
-
-    /**
-     * Which PID slot to pull gains from. Starting 2018, you can choose from
-     * 0,1,2 or 3. Only the first two (0,1) are visible in web-based
-     * configuration.
-     */
-    private static final int kSlotIdx = 0;
-
-    /**
-     * Talon SRX/ Victor SPX will supported multiple (cascaded) PID loops. For
-     * now we just want the primary one.
-     */
-    private static final int kPIDLoopIdx = 0;
-
-    /**
-     * Set to zero to skip waiting for confirmation, set to nonzero to wait and
-     * report to DS if action fails.
-     */
-    private static final int kTimeoutMs = 30;
-
-    /**
-     * PID Gains may have to be adjusted based on the responsiveness of control
-     * loop.
-     * kF: 1023 represents output value to Talon at 100%, 7200 represents Velocity
-     * units at 100% output
-     * 
-     * kP kI kD kF Iz PeakOut
-     */
-    private final Gains kGains_Velocit = new Gains(0.25, 0.001, 20, 1023.0 / 7200.0, 300, 1.00);
-
-    TalonSRX talonLead = new WPI_TalonSRX(1);
+    DigitalInput upperSensor = new DigitalInput(1);
+    DigitalInput lowerSensor = new DigitalInput(1);
 
     /** Creates a new FeederSubsystem. */
     public FeederSubsystem() {
-        talonLead.configFactoryDefault();
-        /* Config sensor used for Primary PID [Velocity] */
-        talonLead.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative,
-                kPIDLoopIdx,
-                kTimeoutMs);
-
-        /**
-         * Phase sensor accordingly.
-         * Positive Sensor Reading should match Green (blinking) Leds on Talon
-         */
-        talonLead.setSensorPhase(false);
-
-        /* Config the peak and nominal outputs */
-        talonLead.configNominalOutputForward(0, kTimeoutMs);
-        talonLead.configNominalOutputReverse(0, kTimeoutMs);
-        talonLead.configPeakOutputForward(1, kTimeoutMs);
-        talonLead.configPeakOutputReverse(-1, kTimeoutMs);
-
-        /* Config the Velocity closed loop gains in slot0 */
-        talonLead.config_kF(kPIDLoopIdx, kGains_Velocit.kF, kTimeoutMs);
-        talonLead.config_kP(kPIDLoopIdx, kGains_Velocit.kP, kTimeoutMs);
-        talonLead.config_kI(kPIDLoopIdx, kGains_Velocit.kI, kTimeoutMs);
-        talonLead.config_kD(kPIDLoopIdx, kGains_Velocit.kD, kTimeoutMs);
     }
 
     @Override
@@ -96,18 +28,38 @@ public class FeederSubsystem extends SubsystemBase {
         // This method will be called once per scheduler run
     }
 
-    public void startFeeder(double rpm) {
-        // 4096 encoder ticks per revolution / 100 ms / minute
-        double targetVelocity_UnitsPer100ms = rpm * 4096 / 600;
-
-        talonLead.set(ControlMode.Velocity, targetVelocity_UnitsPer100ms);
+    public void setUpperFeederSpeed(double speed) {
+        upperFeeder.set(ControlMode.PercentOutput, speed);
     }
 
-    public void startFeeder() {
-        startFeeder(DEFAULT_FEEDER_RPM);
+    public void setLowerFeederSpeed(double speed) {
+        lowerFeeder.set(ControlMode.PercentOutput, speed);
     }
 
-    public void stopFeeder() {
-        talonLead.set(ControlMode.Velocity, 0);
+    public boolean getUpperSensor() {
+        return upperSensor.get();
+    }
+
+    public boolean getLowerSensor() {
+        return lowerSensor.get();
+    }
+
+    public boolean runUntilUpperSensor(double speed) {
+        if (!getUpperSensor()) {
+            setUpperFeederSpeed(speed);
+        } else {
+            setUpperFeederSpeed(0);
+        }
+        return getUpperSensor();
+    }
+
+    public boolean runUntilLowerSensor(double speed) {
+        //run lower until lower is hit but ignore if upper is not hit yet
+        if (!getLowerSensor() && getUpperSensor()) {
+            setLowerFeederSpeed(speed);
+        } else {
+            setLowerFeederSpeed(0);
+        }
+        return getLowerSensor();
     }
 }
