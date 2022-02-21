83cd8d00b48295d6c9f2386a9fac3128519ec486
NeuralNetBot <console.logisaiah@gmail.com>
2022-02-21 13:22:48 -0800

    added lift limit switches, encoder limits and approach slowdown

--- src/main/java/frc/robot/commands/LiftCommand.java
+++ src/main/java/frc/robot/commands/LiftCommand.java
@@ -13,6 +13,10 @@ public class LiftCommand extends CommandBase {
     private final LiftSubsystem liftSubsystem;
     private final DoubleSupplier trigger;
     private final DoubleSupplier leftJoystickY;
+    private final double UPPER_ENDPOINT = 20.0;
+    private final double LOWER_ENDPOINT = 0.0;
+    private final double APPROACH_MAX_SPEED = 0.2;
+
 
     /** Creates a new LiftCommand. */
     public LiftCommand(LiftSubsystem liftSubsystem, DoubleSupplier trigger, DoubleSupplier leftJoystickY) {
@@ -27,13 +31,28 @@ public class LiftCommand extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
+        liftSubsystem.setLiftMaximumPosition(LOWER_ENDPOINT, UPPER_ENDPOINT);
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
         if (trigger.getAsDouble() > .5) {
-            liftSubsystem.setLiftSpeed(leftJoystickY.getAsDouble());
+            double liftSpeed = leftJoystickY.getAsDouble();
+            if (liftSubsystem.getLiftEncoderPostion() - LOWER_ENDPOINT < 3 || UPPER_ENDPOINT - liftSubsystem.getLiftEncoderPostion() < 3) {
+                liftSpeed = Math.min(Math.max(liftSpeed, APPROACH_MAX_SPEED), -APPROACH_MAX_SPEED);
+            }
+            if (liftSubsystem.getLowerSwitch() && liftSpeed > 0) {
+                liftSubsystem.setLiftSpeed(liftSpeed);
+            } else if(liftSubsystem.getUpperSwitch() && liftSpeed < 0) {
+                liftSubsystem.setLiftSpeed(liftSpeed);
+            } else if (!liftSubsystem.getLowerSwitch() && !liftSubsystem.getUpperSwitch()) {
+                liftSubsystem.setLiftSpeed(liftSpeed);
+            } else {
+                liftSubsystem.setLiftSpeed(0);
+            }
+            
+        
         } else {
             liftSubsystem.setLiftSpeed(0);
         }
