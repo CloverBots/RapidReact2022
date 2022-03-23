68cb0321775aa34c995baff547c28fae909e291a
lindegen000 <97714741+lindegen000@users.noreply.github.com>
2022-03-22 19:18:05 -0700

    Added Ball Inflation Factor and Max RPM (#75)
    Co-authored-by: savortheflavor <savortheflavor.gg@gmail.com>

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -77,6 +77,10 @@ public class ShooterSubsystem extends SubsystemBase {
     }
 
     public void setShooterRPM(double rpm) {
+        if (rpm > MAX_RPM)
+        {
+            rpm = MAX_RPM;
+        }
 
         /**
          * PIDController objects are commanded to a set point using the
