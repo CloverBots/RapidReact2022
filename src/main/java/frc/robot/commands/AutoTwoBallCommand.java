2d3ca3df891aa11b98689823f81aeafe6547518a
Fall Gold <92420381+Fall-Gold@users.noreply.github.com>
2022-03-28 19:30:35 -0700

    Ball inflation factor fixes
    Lowered rpm by 80

--- src/main/java/frc/robot/commands/AutoTwoBallCommand.java
+++ src/main/java/frc/robot/commands/AutoTwoBallCommand.java
@@ -36,7 +36,7 @@ public class AutoTwoBallCommand extends SequentialCommandGroupExtended {
         addInstant(() -> lowerFeederSubsystem.setSpeed(0), lowerFeederSubsystem);
         addCommands(new DriveToDistanceCommand(driveSubsystem, -1.2, DRIVE_SPEED, DRIVE_ROTATE, 0.08));
         addCommands(new AutoAlignHighCommand(driveSubsystem, visionTargetTracker, 1));
-        addInstant(() -> shooterSubsystem.setShooterRPM(4200));
+        addInstant(() -> shooterSubsystem.setShooterRPM(4120));
         addCommands(new WaitCommand(1));
         addInstant(() -> lowerFeederSubsystem.setSpeed(1), lowerFeederSubsystem);
         addInstant(() -> upperFeederSubsystem.setSpeed(1), upperFeederSubsystem);
